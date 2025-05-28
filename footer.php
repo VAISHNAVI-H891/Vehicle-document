46e846a24f1066fe1942ed047726882242eb528b
Vaishnavi Hujaratti <vaishnavihujaratti@gmail.com>
2025-05-28 12:30:53 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+  <div align="center">
+    <strong>Copyright &copy; 2025 <a href="">WELCOME TO QR CODE BASED DOCUMENT VERIFICATION OF VEHICLES.</a>.</strong> All rights
+    reserved.
+    </div>
+  </footer>
+  
+  
+  
